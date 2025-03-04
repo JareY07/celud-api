087a2aa2d6a96dfe8a0f1be66246a01805a9d9f1
JareY07 <jarey1715@gmail.com>
2025-03-03 20:54:27 -0600

    update

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
 engine = create_engine(
-    "mysql+pymysql://root:root@localhost:3306/db")
+    "mysql+pymysql://root:password@localhost:3306/system_health_mobile")
 
 meta = MetaData()
