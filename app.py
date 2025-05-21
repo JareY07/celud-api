c8b8d8687dc0bfd9e1baa957d14a3c78bab2cf8b
JareY07 <jarey1715@gmail.com>
2025-05-21 01:59:03 -0600

    A new beggining

--- app.py
+++ app.py
@@ -14,17 +14,9 @@ app.version = "0.0.5"
 
 templates = Jinja2Templates(directory="templates")
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://127.0.0.1:5500"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
 engine = create_engine(
-    "mysql+pymysql://root:password@localhost:3306/system_health_mobile")
+    "mysql+pymysql://root:password@localhost:3306/prueba_salud_movil")
 
 meta = MetaData()
